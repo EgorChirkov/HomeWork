b7a975ce85bbabf1ad36a72f73b954ae9ffcf1af
Egor <egorchirkov03@gmail.com>
2022-06-11 11:28:10 +0300

    Initial Commit

---
+++ HomeWork/HomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkApp.swift
+//  HomeWork
+//
+//  Created by Егор Чирков on 11.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
